9850adc1708b4db0c691865b02097f7af90b6c92
Dimych <kuzyuberdin@gmail.com>
2024-03-29 17:23:25 +0400

    add baseDomainEvent

--- wallets/src/features/clients/domain/entities/client/events/client-updated.event.ts
+++ wallets/src/features/clients/domain/entities/client/events/client-updated.event.ts
@@ -1,6 +1,32 @@
 import { UpdateClientCommand } from '../client.entity';
 
-export class ClientUpdatedEvent {
+export type DomainEventMeta =
+    {
+      type: string,
+      date: Date,
+      userId: string | null,
+      requestId: string
+    }
+
+export abstract class DomainEvent {
+  private type: string;
+
+  public meta: DomainEventMeta
+
+  constructor(type: string) {
+    this.type = type;
+  }
+
+  public setMeta(meta: DomainEventMeta) {
+    this.meta = {
+      ...meta,
+      type: this.type
+    };
+  }
+}
+
+export class ClientUpdatedEvent extends DomainEvent {
+  static type = 'finance/wallet/client-updated';
   public clientId: string;
 
   public firstName?: string;
@@ -8,6 +34,7 @@ export class ClientUpdatedEvent {
   public address?: string | null;
 
   constructor(clientId: string, command: UpdateClientCommand) {
+    super(ClientUpdatedEvent.type);
     this.clientId = clientId;
 
     if (typeof command.address !== 'undefined') {
